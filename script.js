1053f1750a4c146045c0bb96717ca448c945155e
Abhay-0103 <ab0321054@gmail.com>
2025-08-21 00:22:10 +0530

    added from and submit

--- script.js
+++ script.js
@@ -43,4 +43,34 @@ function typeEffect() {
 typeEffect();
 
 
+ window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+  /* customize formbutton below*/     
+  formbutton("create", {
+    action: "https://formspree.io/f/xovloyjr",
+    title: "How can we help?",
+    fields: [
+      { 
+        type: "email", 
+        label: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "#ea500c"
+      },
+      button: {
+        backgroundColor: "#ea500c"
+      }
+    }
+  });
 
