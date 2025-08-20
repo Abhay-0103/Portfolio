5f2353dcc13a165b61b90689018428f316cfb676
Abhay-0103 <ab0321054@gmail.com>
2025-08-21 00:36:27 +0530

    navbar fixed

--- script.js
+++ script.js
@@ -74,3 +74,9 @@ typeEffect();
     }
   });
 
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('nav ul');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
